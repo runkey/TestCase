40f456ec35a16647d59a82767b14205a315f19da
kiran <kiran@192.168.1.2>
2018-01-15 18:57:37 -0800

    changes

--- SeleniumWork/src/test/java/sampletests/GeckoDriverTest.java
+++ SeleniumWork/src/test/java/sampletests/GeckoDriverTest.java
@@ -1,39 +1,19 @@
 package sampletests;
-
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import DriverUtility.BasceScript;
 
-public class GeckoDriverTest
+public class GeckoDriverTest extends BasceScript
 
 {
  
-  WebDriver driver;
-  
-  @BeforeClass
-  public void beforeClass() 
-  {
+ @Test
  
-	 System.setProperty("webdriver.gecko.driver","D:\\GeckoDriver\\geckodriver.exe");  
-     driver=new FirefoxDriver();
-  }
-  
-  @Test
-  public void testCase()
-  
-  {
-	  
-	  driver.get("http://automationpractice.com/");
-	  
-  }
-  @AfterClass
-  public void afterClass() 
-  {
-	  
-	driver.quit();
-  }
+ public void testMethod1()
+ {
+	 
+	driver.get("http://www.google.com");
+	Assert.assertEquals(driver.getTitle(),"test");
+ }
 
 }
